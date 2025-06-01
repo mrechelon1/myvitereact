e348f1de371f8b028808354c7ccc25077d44b1fd
mrechelon1 <139581772+mrechelon1@users.noreply.github.com>
2025-06-01 09:18:05 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // or '/your-app/' or the appropriate base URL
+ base: "/deploy_react_app_github_pages_vercel",
   build: {
     // ... other build options
   },
